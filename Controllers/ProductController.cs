7987ed0331ac8e3d34bf158229ef5d7831c267cf
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2023-10-16 12:17:41 +0100

    Added product solution code

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WeatherDemoApi.Models;
+
+namespace WeatherDemoApi.Controllers
+{
+   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly Product product = new Product();
+
+        [HttpGet("list")]
+
+        public List<string> GetList()
+        {
+
+            return product.List();
+        }
+
+        [HttpGet("item/{id}")]
+
+        public string GetItem(int id)
+        {
+
+            return product.List().ToArray()[id];
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace WeatherDemoApi.Models
+{
+    public abstract class AbstractProduct
+    {
+    }
+    public class Product : AbstractProduct
+    {
+        public List<string> List()
+        {
+            return new List<string> { "Ladder", "Ball", "Coffee" };
+        }
+    }
+}
