d59582064aa156355c8bc0965c497586bde8be99
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2023-10-16 12:00:11 +0100

    Add project files.

---
+++ Controllers/MagicEightBallController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WeatherDemoApi.Models;
+
+namespace WeatherDemoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MagicEightBallController : ControllerBase
+    {
+        private readonly Magic8Ball magic8Ball = new Magic8Ball();
+
+        public MagicEightBallController()
+        {
+            
+        }
+
+
+        [HttpGet("list")]
+        
+        public List<string> GetList()
+        {
+        
+            return magic8Ball.predictions();
+        }
+
+        [HttpGet("prediction")]
+
+        public string GetPrediction()
+        {
+            Random rnd = new Random();
+          
+
+            List<string> predictions = magic8Ball.predictions();
+
+            int predictionIndex = rnd.Next(0, predictions.Count); // creates a number between 1 and 12
+          
+            return predictions.ToArray()[predictionIndex];
+        }
+
+        [HttpGet("prediction/{id}")]
+
+        public string GetSpecificPrediction(int id)
+        {
+           
+            return magic8Ball.predictions().ToArray()[id];
+        }
+    }
+}
---
+++ Models/PredictionEngine.cs
@@ -0,0 +1,18 @@
+﻿namespace WeatherDemoApi.Models
+{
+    public abstract class PredictionEngine
+    {
+    }
+
+    public class Magic8Ball  : PredictionEngine { 
+  
+        public Magic8Ball() { }
+
+        public List<string> predictions()
+        {
+            return new List<string> { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+        }
+
+    
+    }
+}
